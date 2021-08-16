24862b7f78b69014df4ff0a6f021dd60d17b0f81
Fernando74lr <flopezramirez@hotmail.com>
2021-08-15 22:10:00 -0500

    Added EP_1.5

---
+++ Introduction/EP/ep_1_5.cs
@@ -0,0 +1,85 @@
+using System;
+
+class ER_1_5
+{
+    static void Main()
+    {
+        // First Line
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("     ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("      \n");
+        
+        // Second Line
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("      ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("  \n");
+
+        // Third Line
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("     ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("     ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(" \n");
+        
+        // Fourth Line
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("      ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("  \n");
+        
+        // Fifth Line
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("     ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("     ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(" \n");
+        
+        // Sixth Line
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("      ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("  \n");
+        
+        // Seventh Line
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(" ");
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("     ");
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write("      \n");
+    }
+}
