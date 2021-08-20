321901132383c0712940d37312ee093268c81504
Fernando74lr <flopezramirez@hotmail.com>
2021-08-20 14:26:47 -0500

    ER 5.5

---
+++ Introduction/ER/ER_5/er_5_5.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+class ER_5_5
+{
+    static void Main()
+    {
+        // Inicializar matriz con datos y matriz vacía.
+
+        int n = 10;
+
+        for (int i=0; i < n; i++)
+        {
+            Console.WriteLine("Introduzca el número");
+        }
+        
+    }
+}
