96010f04d00ea881c234208c78c017b829003b9a
Fernando74lr <flopezramirez@hotmail.com>
2021-08-27 15:08:57 -0500

    Added First VS Community Project

---
+++ VS Community/EjemploSencilloClases/EjemploSencilloClases/Automovil.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace EjemploSencilloClases
+{
+    class Automovil
+    {
+        // Atributos del Automóvil.
+        private string modelo;
+        private string color;
+        private int año;
+
+        // Constructor sin parámatros.
+        public Automovil()
+        {
+
+        }
+
+        // Constructor con parámatros.
+        public Automovil(string Modelo, string Color, int Año)
+        {
+            modelo = Modelo;
+            color = Color;
+            año = Año;
+        }
+
+        // Métodos para asignar valores a los atributos.
+        public void AsignarModelo(string Modelo)
+        {
+            modelo = Modelo;
+        }
+
+        public void AsignarColor(string Color)
+        {
+            color = Color;
+        }
+
+        public void AsignarAño(int Año)
+        {
+            año = Año;
+        }
+
+        // Métodos para obtener los atributos.
+        public string ObtenerModelo()
+        {
+            return modelo;
+        }
+
+        public string ObtenerColor()
+        {
+            return color;
+        }
+
+        public int ObtenerAño()
+        {
+            return año;
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public int Año
+        {
+            get { return año; }
+            set { año = value; }
+        }
+
+    }
+}
---
+++ VS Community/EjemploSencilloClases/EjemploSencilloClases/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EjemploSencilloClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Audi = new Automovil("R8", "Negro", 2021);
+            var VW = new Automovil();
+
+            VW.AsignarModelo("Jetta");
+            VW.AsignarColor("Azul");
+            VW.AsignarAño(2021);
+
+            //Console.WriteLine($"{VW.getColor()}");
+            string miModelo = VW.ObtenerModelo();
+            string miColor = VW.ObtenerColor();
+            int miAño = VW.ObtenerAño();
+
+            Console.WriteLine($"VW. Modelo: {miModelo}, Color: {miColor}, Año: {miAño}.");
+            Console.WriteLine($"Jetta. Modelo: {Audi.Modelo}, Color: {Audi.Color}, Año: {Audi.Año}.");
+
+
+            Console.Write("Presione cualquier tecla para terminar");
+            Console.ReadKey();
+        }
+    }
+}
