4cbeeff7fe405b665c626fa28649cea50b381c61
Fernando74lr <flopezramirez@hotmail.com>
2021-09-14 16:14:39 -0500

    Serial Port TX

---
+++ VS Community/SerialPortTX/SerialPortTX/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SerialPortTX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSearchPorts = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnOut = new System.Windows.Forms.Button();
+            this.txtDataTX = new System.Windows.Forms.TextBox();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.spSerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.cboBaudrate = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnSearchPorts
+            // 
+            this.btnSearchPorts.Location = new System.Drawing.Point(38, 28);
+            this.btnSearchPorts.Name = "btnSearchPorts";
+            this.btnSearchPorts.Size = new System.Drawing.Size(90, 23);
+            this.btnSearchPorts.TabIndex = 0;
+            this.btnSearchPorts.Text = "Buscar puertos";
+            this.btnSearchPorts.UseVisualStyleBackColor = true;
+            this.btnSearchPorts.Click += new System.EventHandler(this.btnSearchPorts_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(345, 28);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(89, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Conectar";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(38, 183);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnOut
+            // 
+            this.btnOut.Location = new System.Drawing.Point(359, 233);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(75, 23);
+            this.btnOut.TabIndex = 3;
+            this.btnOut.Text = "&Salir";
+            this.btnOut.UseVisualStyleBackColor = true;
+            this.btnOut.Click += new System.EventHandler(this.btnOut_Click);
+            // 
+            // txtDataTX
+            // 
+            this.txtDataTX.Location = new System.Drawing.Point(119, 183);
+            this.txtDataTX.Name = "txtDataTX";
+            this.txtDataTX.Size = new System.Drawing.Size(315, 20);
+            this.txtDataTX.TabIndex = 4;
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(134, 28);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 21);
+            this.cboPorts.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Baud Rate";
+            // 
+            // cboBaudrate
+            // 
+            this.cboBaudrate.FormattingEnabled = true;
+            this.cboBaudrate.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "4800",
+            "9600",
+            "28800",
+            "38400",
+            "57600"});
+            this.cboBaudrate.Location = new System.Drawing.Point(134, 58);
+            this.cboBaudrate.Name = "cboBaudrate";
+            this.cboBaudrate.Size = new System.Drawing.Size(121, 21);
+            this.cboBaudrate.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 278);
+            this.Controls.Add(this.cboBaudrate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.txtDataTX);
+            this.Controls.Add(this.btnOut);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSearchPorts);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Puerto Serie TX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearchPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.TextBox txtDataTX;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort spSerialPort;
+        private System.Windows.Forms.ComboBox cboBaudrate;
+    }
+}
+
---
+++ VS Community/SerialPortTX/SerialPortTX/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialPortTX
+{
+    public partial class Form1 : Form
+    {
+
+        // Cadena de texto para enviar
+        private string strBufferOut;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            strBufferOut = "";
+            btnSend.Enabled = false;
+        }
+
+        private void btnOut_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSearchPorts_Click(object sender, EventArgs e)
+        {
+            string[] puertosDisponibles = SerialPort.GetPortNames();
+            cboPorts.Items.Clear();
+
+            foreach (string puerto in puertosDisponibles)
+            {
+                cboPorts.Items.Add(puerto);
+            }
+
+            if (cboPorts.Items.Count > 0)
+            {
+                cboPorts.SelectedIndex = 3;
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btnConnect.Text == "Conectar")
+                {
+                    spSerialPort.BaudRate = Int32.Parse(cboBaudrate.Text);
+                    spSerialPort.DataBits = 8;
+                    spSerialPort.Parity = Parity.None;
+                    spSerialPort.StopBits = StopBits.One;
+                    spSerialPort.Handshake = Handshake.None;
+                    spSerialPort.PortName = cboPorts.Text;
+
+                    try
+                    {
+                        spSerialPort.Open();
+                        btnConnect.Text = "Desconectar";
+                        btnSend.Enabled = true;
+                        cboPorts.Enabled = false;
+                        cboBaudrate.Enabled = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else if (btnConnect.Text == "Desconectar")
+                {
+                    spSerialPort.Close();
+                    btnConnect.Text = "Conectar";
+                    btnSend.Enabled = false;
+                    cboBaudrate.Enabled = true;
+                    cboPorts.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void cboBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
