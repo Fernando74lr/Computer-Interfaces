233f295a020f30cd93122f49b51f56e3e9efa7b9
Fernando74lr <flopezramirez@hotmail.com>
2021-10-03 17:59:14 -0500

    Serial Port TX/RX (bytes)

---
+++ VS Community/SerialPortTXRXbytes/SerialPortTXRXbytes/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SerialPortTXRXbytes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbValueToSend = new System.Windows.Forms.TextBox();
+            this.tbReceivedValue = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.spSerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(148, 17);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 0;
+            this.tbPort.Text = "COM60";
+            this.tbPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbValueToSend
+            // 
+            this.tbValueToSend.Enabled = false;
+            this.tbValueToSend.Location = new System.Drawing.Point(148, 96);
+            this.tbValueToSend.Name = "tbValueToSend";
+            this.tbValueToSend.Size = new System.Drawing.Size(100, 20);
+            this.tbValueToSend.TabIndex = 1;
+            this.tbValueToSend.Text = "0";
+            this.tbValueToSend.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbReceivedValue
+            // 
+            this.tbReceivedValue.Enabled = false;
+            this.tbReceivedValue.Location = new System.Drawing.Point(148, 179);
+            this.tbReceivedValue.Name = "tbReceivedValue";
+            this.tbReceivedValue.Size = new System.Drawing.Size(100, 20);
+            this.tbReceivedValue.TabIndex = 2;
+            this.tbReceivedValue.Text = "0";
+            this.tbReceivedValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(148, 44);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "&Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(148, 122);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "&Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(148, 205);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Serial port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Value to send;:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Received Value";
+            // 
+            // spSerialPort
+            // 
+            this.spSerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.spSerialPort_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 237);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbReceivedValue);
+            this.Controls.Add(this.tbValueToSend);
+            this.Controls.Add(this.tbPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serial Port TX/RX (bytes)";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbValueToSend;
+        private System.Windows.Forms.TextBox tbReceivedValue;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort spSerialPort;
+    }
+}
+
---
+++ VS Community/SerialPortTXRXbytes/SerialPortTXRXbytes/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialPortTXRXbytes
+{
+    public partial class Form1 : Form
+    {
+        // Almacenará # total de bytes recibidos
+        int serialBytesToRead = 0;
+        // Buffer para almacenar los bytes recibidos
+        byte[] byteFrame = new byte[64];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "&Connect")
+            {
+                spSerialPort.PortName = tbPort.Text;
+                spSerialPort.Open();
+                btnConnect.Text = "Dis&connect";
+                tbPort.Enabled = false;
+                tbValueToSend.Enabled = true;
+            }
+            else
+            {
+                spSerialPort.Close();
+                tbPort.Enabled = true;
+                btnConnect.Text = "&Connect";
+                tbValueToSend.Enabled = false;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] byteToSend = new byte[1];
+            byteToSend[0] = (byte)Convert.ToInt32(tbValueToSend.Text);
+            spSerialPort.Write(byteToSend, 0, 1);
+        }
+
+        private void spSerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            serialBytesToRead = spSerialPort.BytesToRead;
+
+            for (int i=0; i < serialBytesToRead; i++)
+            {
+                byteFrame[i] = (byte)spSerialPort.ReadByte();
+            }
+            this.Invoke(new EventHandler(displayDataReceived));
+        }
+
+        private void displayDataReceived(object o, EventArgs e)
+        {
+            for (int i=0; i < serialBytesToRead; i++)
+            {
+                tbReceivedValue.Text = Convert.ToString(byteFrame[i]);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            spSerialPort.Close();
+            this.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            spSerialPort.Close();
+        }
+    }
+}
